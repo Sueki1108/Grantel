400cbb96ce9f8391ba776951fde31489c20d3153
Thiago Sueki <thiago.suek@gmail.com>
2025-10-16 19:40:21 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,19 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBVi6N-Linrk9k6z9vO-Z36XmtsD9QKwwA",
-  authDomain: "grantel-1oth2.firebaseapp.com",
-  projectId: "grantel-1oth2",
-  storageBucket: "grantel-1oth2.appspot.com",
-  messagingSenderId: "698741029963",
-  appId: "1:698741029963:web:538fab20dc90e3eedde1ce"
+export const firebaseConfig = {
+  "projectId": "grantel-1oth2",
+  "appId": "1:698741029963:web:538fab20dc90e3eedde1ce",
+  "apiKey": "AIzaSyBVi6N-Linrk9k6z9vO-Z36XmtsD9QKwwA",
+  "authDomain": "grantel-1oth2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "698741029963"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export { app, firebaseConfig };
