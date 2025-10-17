63376f635b6d5549b3294de7817e6c5316034f32
Thiago Sueki <thiago.suek@gmail.com>
2025-10-17 17:36:57 +0000

    Apelido do app grantel ID do aplicativo 1:698741029963:web:11155a6c80a65

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "grantel-1oth2",
-  "appId": "1:698741029963:web:538fab20dc90e3eedde1ce",
-  "apiKey": "AIzaSyBVi6N-Linrk9k6z9vO-Z36XmtsD9QKwwA",
-  "authDomain": "grantel-1oth2.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "698741029963"
+  apiKey: "AIzaSyBVi6N-Linrk9k6z9vO-Z36XmtsD9QKwwA",
+  authDomain: "grantel-1oth2.firebaseapp.com",
+  databaseURL: "https://grantel-1oth2-default-rtdb.firebaseio.com",
+  projectId: "grantel-1oth2",
+  storageBucket: "grantel-1oth2.firebasestorage.app",
+  messagingSenderId: "698741029963",
+  appId: "1:698741029963:web:11155a6c80a65859dde1ce"
 };
