7857d800d6e40802d3fe095078525cb5a109f194
Thiago Sueki <thiago.suek@gmail.com>
2025-10-22 13:11:46 +0000

    Compressing objects: 100% (10/10), done. Writing objects: 100% (10/10),

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const {https} = require('firebase-functions');
+const {default: next} = require('next');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+const server = next({
+  dev: isDev,
+  // Location of the Next.js project
+  conf: {distDir: '.next'},
+});
+
+const nextjsHandle = server.getRequestHandler();
+
+exports.server = https.onRequest((req, res) => {
+  // log the page.js file path
+  // This log is output to the Firebase Functions logs
+  console.log('File: ' + req.originalUrl);
+  return server.prepare().then(() => nextjsHandle(req, res));
+});
