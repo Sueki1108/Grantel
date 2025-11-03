1a81eda67d1a2d9620cb1653232dac2a0f624eb9
Thiago Sueki <thiago.suek@gmail.com>
2025-11-03 10:51:17 +0000

    studio-2131033956:~/studio{main}$ git push Enumerating objects: 15, done

--- functions/index.js
+++ functions/index.js
@@ -1,39 +1,18 @@
-"use strict";
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/hhttps");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const next = require("next");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const next = require('next');
-
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 
 const server = next({
   dev: isDev,
   // Location of the Next.js project
-  conf: {distDir: '.next'},
+  conf: { distDir: ".next" },
 });
 
 const nextjsHandle = server.getRequestHandler();
 
-exports.server = onRequest((req, res) => {
-  // log the page.js file path
-  // This log is output to the Firebase Functions logs
-  console.log('File: ' + req.originalUrl);
+exports.server = functions.https.onRequest((req, res) => {
+  console.log("File: " + req.originalUrl);
   return server.prepare().then(() => nextjsHandle(req, res));
 });
