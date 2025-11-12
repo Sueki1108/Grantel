ebbb4c7f4116a951790944bab570a353f59a5028
Thiago Sueki <thiago.suek@gmail.com>
2025-11-12 19:05:54 +0000

    na janela que abre para mostrar o prcessamento em guia difal nao tem um

--- src/components/app/difal-analysis.tsx
+++ src/components/app/difal-analysis.tsx
@@ -6,7 +6,7 @@ import * as XLSX from 'xlsx';
 import { useToast } from "@/hooks/use-toast";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
-import { FileUp, Loader2, Download, Cpu, TicketPercent, Copy, Calendar as CalendarIcon, Hash, Sigma, Coins, ClipboardCopy } from 'lucide-react';
+import { FileUp, Loader2, Download, Cpu, TicketPercent, Copy, Calendar as CalendarIcon, Hash, Sigma, Coins, ClipboardCopy, X } from 'lucide-react';
 import JSZip from 'jszip';
 import { cn } from '@/lib/utils';
 import { format, parseISO } from 'date-fns';
@@ -16,7 +16,7 @@ import { getColumnsWithCustomRender } from "@/components/app/columns-helper";
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { Calendar } from '../ui/calendar';
 import { Label } from '../ui/label';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, DialogFooter } from '../ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, DialogFooter, DialogClose } from '../ui/dialog';
 
 // ===============================================================
 // Tipos
@@ -355,11 +355,13 @@ export function DifalAnalysis() {
                             <Download className="mr-2 h-4 w-4" /> Baixar Excel
                         </Button>
                     </DialogFooter>
+                     <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+                        <X className="h-4 w-4" />
+                        <span className="sr-only">Close</span>
+                    </DialogClose>
                 </DialogContent>
             </Dialog>
 
         </div>
     );
 }
-
-    
