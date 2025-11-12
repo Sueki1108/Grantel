fdce26d8c466f6d4ae2784149f8deb606dcf10f6
Thiago Sueki <thiago.suek@gmail.com>
2025-11-12 18:04:16 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/app/automator/page.tsx
+++ src/app/automator/page.tsx
@@ -1,7 +1,19 @@
+import dynamic from 'next/dynamic'
+import { Skeleton } from '@/components/ui/skeleton'
 
-import { AutomatorClientPage } from './page-client';
-
-export const dynamic = 'force-dynamic'
+const AutomatorClientPage = dynamic(
+  () => import('./page-client').then((mod) => mod.AutomatorClientPage),
+  {
+    ssr: false,
+    loading: () => (
+        <div className="p-4 md:p-8 space-y-8">
+            <Skeleton className="h-10 w-full" />
+            <Skeleton className="h-64 w-full" />
+            <Skeleton className="h-32 w-full" />
+        </div>
+    ),
+  }
+)
 
 export default function AutomatorPage() {
   return <AutomatorClientPage />;
