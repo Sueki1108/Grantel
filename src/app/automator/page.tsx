9358e98d72740d6c0c7813722e1182e13cd39a6d
Thiago Sueki <thiago.suek@gmail.com>
2025-11-18 11:22:39 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/automator/page.tsx
+++ src/app/automator/page.tsx
@@ -1,19 +1,4 @@
-import dynamic from 'next/dynamic'
-import { Skeleton } from '@/components/ui/skeleton'
-
-const AutomatorClientPage = dynamic(
-  () => import('./page-client').then((mod) => mod.AutomatorClientPage),
-  {
-    ssr: false,
-    loading: () => (
-        <div className="p-4 md:p-8 space-y-8">
-            <Skeleton className="h-10 w-full" />
-            <Skeleton className="h-64 w-full" />
-            <Skeleton className="h-32 w-full" />
-        </div>
-    ),
-  }
-)
+import { AutomatorClientPage } from './page-client';
 
 export default function AutomatorPage() {
   return <AutomatorClientPage />;
